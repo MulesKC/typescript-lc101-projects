e0b23a62e0abd430be1457cec1e1f9614700dd2f
MulesKC <rmmcp3@gmail.com>
2019-12-09 19:07:20 -0600

    finished basic framework

---
+++ studio/rocket.ts
@@ -0,0 +1,17 @@
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: array;
+    astronauts: array; 
+
+    constructor (name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+
+    }
+
+    currentMassKg(): 
+}
